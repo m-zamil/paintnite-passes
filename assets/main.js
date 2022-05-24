9d524894507f8a7622b0d6ee54e87190b1d282fe
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-05-24 22:52:27 +0500

    completed

---
+++ assets/main.js
@@ -0,0 +1,77 @@
+LottieInteractivity.create({
+  player: "#worksLottieM",
+  mode: "scroll",
+  container: "#worksAnimationPlay",
+  actions: [
+    {
+      visibility: [0, 1],
+      type: "play",
+      frames: [25, 100],
+    },
+  ],
+});
+
+LottieInteractivity.create({
+  player: "#worksLottieD",
+  mode: "scroll",
+  container: "#worksAnimationPlay",
+  actions: [
+    {
+      visibility: [0, 1],
+      type: "play",
+      frames: [25, 100],
+    },
+  ],
+});
+
+LottieInteractivity.create({
+  player: "#orange__circlePlayer",
+  mode: "scroll",
+  container: "#orange__circleCont",
+  actions: [
+    {
+      visibility: [0, 1],
+      type: "play",
+      frames: [0, 100],
+    },
+  ],
+});
+
+LottieInteractivity.create({
+  player: "#apronPlayer",
+  mode: "scroll",
+  container: "#apronContainer",
+  actions: [
+    {
+      visibility: [0, 1],
+      type: "play",
+      frames: [0, 100],
+    },
+  ],
+});
+
+LottieInteractivity.create({
+  player: "#redeemBrushPlayer",
+  mode: "scroll",
+  container: "#redeemBrushCont",
+  actions: [
+    {
+      visibility: [0, 1],
+      type: "play",
+      frames: [50, 75],
+    },
+  ],
+});
+
+LottieInteractivity.create({
+  player: "#greencirclePlayer",
+  mode: "scroll",
+  container: "#greencirclePlayer",
+  actions: [
+    {
+      visibility: [0, 1],
+      type: "play",
+      frames: [0, 100],
+    },
+  ],
+});
